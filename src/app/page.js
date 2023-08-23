bf261eef2e219d53f11a10f6887250273a4ff8a1
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-08-23 14:55:23 +0630

    add reverse animation

--- src/app/page.js
+++ src/app/page.js
@@ -19,6 +19,8 @@ export default function Home({posts}) {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
           entry.target.classList.add('active');
+        } else {
+          entry.target.classList.remove('active');
         }
       });
     }, options);
